dcb760464b522f5b136b71f956666fa38dbceccf
Andre-Franco1 <andrefranco3172@gmail.com>
2025-07-18 22:17:44 -0300

    refactor: making the code modular

--- modules.tf
+++ modules.tf
@@ -2,7 +2,7 @@ module "eks_network" {
   source       = "./modules/network"
   cidr_block   = var.cidr_block
   project_name = var.project_name
-  tags         = local.tags
+  tags         = var.tags
 }
 
 module "eks_cluster" {
@@ -10,13 +10,13 @@ module "eks_cluster" {
   project_name     = var.project_name
   public_subnet_1a = module.eks_network.subnet_pub_1a
   public_subnet_1b = module.eks_network.subnet_pub_1b
-  tags             = local.tags
+  tags             = var.tags
 }
 
 module "eks_managed_node_group" {
   source            = "./modules/managed-node-group"
   project_name      = var.project_name
-  tags              = local.tags
+  tags              = var.tags
   cluster_name      = module.eks_cluster.cluster_name
   private_subnet_1a = module.eks_network.subnet_priv_1a
   private_subnet_1b = module.eks_network.subnet_priv_1b
@@ -25,7 +25,7 @@ module "eks_managed_node_group" {
 module "eks-aws-load-balancer-controller" {
   source       = "./modules/aws-load-balancer-controller"
   project_name = var.project_name
-  tags         = local.tags
+  tags         = var.tags
   oidc         = module.eks_cluster.oidc
   cluster_name = module.eks_cluster.cluster_name
 }
