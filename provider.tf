0c2eeae553e9adc80bd2f310a682e59704d8c8fa
Andre-Franco1 <andrefranco3172@gmail.com>
2025-07-18 21:19:45 -0300

    feat: deploy helm chart

--- provider.tf
+++ provider.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.37.1"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
   }
   backend "s3" {
     bucket = "comunidadedevops-s3-bucket"
@@ -27,5 +31,17 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
     command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks_cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.cluster_ca_cert)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
   }
 }
